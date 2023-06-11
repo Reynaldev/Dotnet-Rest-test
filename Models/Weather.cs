c14d945b0feba6dbd0f0ca0160413d946aa72852
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-06-11 23:19:58 +0700

    first commit

---
+++ Models/Weather.cs
@@ -0,0 +1,26 @@
+namespace Dotnet_Rest_test.Models
+{
+    public class OpenWeather
+    {
+        public decimal? lat { get; set; }
+        public decimal? lon { get; set; }
+        public string? timezone { get; set; }
+        public Current? current { get; set; }
+    }
+
+    public class Current
+    {
+        public decimal? temp { get; set; }
+        public int? pressure { get; set; }
+        public int? humidity { get; set; }
+        // public Weather weather { get; set; }
+    }
+
+    public class Weather
+    {
+        public int? id { get; set; }
+        public string? main { get; set; }
+        public string? description { get; set; }
+        public string? icon { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Dotnet_Rest_test.Models;
+
+namespace Dotnet_Rest_test
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Initialize OpenWeather class
+            OpenWeather weather = new OpenWeather();
+            // Initialize HttpClient class
+            HttpClient client = new HttpClient();
+
+            // Get REST API content
+            var response = await client.GetAsync("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=18ccbbd129b7bdecaaf072a9f9977f01");
+            // Convert json into string
+            string result = await response.Content.ReadAsStringAsync();
+            // Insert data into the OpenWeather class
+            weather = JsonSerializer.Deserialize<OpenWeather>(result);
+
+            // Output
+            Console.WriteLine($"Temp: {weather.current.temp}");
+            Console.WriteLine($"Pressure: {weather.current.pressure}");
+            Console.WriteLine($"Humidity: {weather.current.humidity}");
+        }
+    }
+}
